b16940ea62481f18ddbb26d012b0f259d4546c9f
James Mueller <jamesloganmueller@gmail.com>
2023-04-21 11:46:32 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,27 +1,42 @@
-import React from "react";
-import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { ImageBackground } from "react-native";
+import { View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}><View style={styles.BWgjvZiv}></View></ScrollView>
-    </SafeAreaView>;
+        
+    <View style={styles.QbpERChc}><ImageBackground style={styles.ZfuTqsxg} source={require("./Runners Background.png")} resizeMode="cover"><ImageBackground style={styles.wqwKpQGP} source={require("./Description.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.tCvMmNvH} source={require("./Time:Connection:Battery.png")} resizeMode="cover"></ImageBackground></ImageBackground></View></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
   },
-  BWgjvZiv: {
-    height: 537,
-    width: 337,
-    backgroundColor: "#E4E4E4",
+  QbpERChc: {
+    height: 572,
+    width: 356,
+    backgroundColor: "#28333F",
     borderRadius: 0,
     color: "#777777"
+  },
+  ZfuTqsxg: {
+    width: 355,
+    height: 572
+  },
+  wqwKpQGP: {
+    width: 252,
+    height: 77,
+    alignItems: "center",
+    position: "absolute",
+    top: 369,
+    left: 51.5
+  },
+  tCvMmNvH: {
+    width: 354,
+    height: 27,
+    position: "absolute",
+    left: -6,
+    top: 7
   }
 });
 export default Untitled1;
