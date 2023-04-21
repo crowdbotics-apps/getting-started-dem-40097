bd12caca152b38550343a6452ee5198eff3bd97e
James Mueller <jamesloganmueller@gmail.com>
2023-04-21 12:34:45 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import { ImageBackground } from "react-native";
 import { View } from "react-native";
 import { StyleSheet, SafeAreaView } from "react-native";
@@ -5,7 +6,7 @@ import { StyleSheet, SafeAreaView } from "react-native";
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
         
-    <View style={styles.QbpERChc}><ImageBackground style={styles.ZfuTqsxg} source={require("./Runners Background.png")} resizeMode="cover"><ImageBackground style={styles.wqwKpQGP} source={require("./Description.png")} resizeMode="cover"><View style={styles.MNIqTKZd}></View></ImageBackground><ImageBackground style={styles.BIIbPkax} source={require("./Home Indicator - On Light.png")} resizeMode="cover"></ImageBackground></ImageBackground></View></SafeAreaView>;
+    <View style={styles.QbpERChc}><ImageBackground style={styles.ZfuTqsxg} source={require("./Runners Background.png")} resizeMode="cover"><ImageBackground style={styles.wqwKpQGP} source={require("./Description.png")} resizeMode="cover"><View style={styles.MNIqTKZd}></View></ImageBackground><ImageBackground style={styles.BIIbPkax} source={require("./Home Indicator - On Light.png")} resizeMode="cover"></ImageBackground><Text style={styles.EmfEkjec}>Get Started</Text></ImageBackground></View></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
@@ -41,12 +42,27 @@ const styles = StyleSheet.create({
   MNIqTKZd: {
     height: 60,
     width: 247,
-    backgroundColor: "#E4E4E4",
-    borderRadius: 0,
+    backgroundColor: "#7B61FF",
+    borderRadius: 10,
     color: "#777777",
     position: "absolute",
     left: 2,
     top: 95
+  },
+  EmfEkjec: {
+    width: 147,
+    height: 62,
+    lineHeight: 50,
+    fontSize: 20,
+    borderRadius: 0,
+    position: "absolute",
+    left: 104,
+    top: 464,
+    fontWeight: "700",
+    color: "#ffffff",
+    opacity: 1,
+    letterSpacing: 0,
+    textAlign: "center"
   }
 });
 export default Untitled1;
