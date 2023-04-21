4b3e6c082fe1ffc11b8f9cfb39da68a42ae56b9a
James Mueller <jamesloganmueller@gmail.com>
2023-04-21 13:42:38 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, SafeAreaView } from "react-native";
 const Untitled3 = () => {
   return <SafeAreaView style={styles.safeArea}>
 
-    <ImageBackground style={styles.ARmhvyuW} source={require("./Home Screen.png")} resizeMode="cover"><ImageBackground style={styles.IdqTpSPL} source={require("./Home Top Info.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.yNoXHbKy} source={require("./Share and get.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.pcJbencr} source={require("./History Section.png")} resizeMode="cover"></ImageBackground></ImageBackground></SafeAreaView>;
+    <ImageBackground style={styles.ARmhvyuW} source={require("./Home Screen.png")} resizeMode="cover"><ImageBackground style={styles.IdqTpSPL} source={require("./Home Top Info.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.yNoXHbKy} source={require("./Share and get.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.pcJbencr} source={require("./History Section.png")} resizeMode="cover"><ImageBackground style={styles.WnohOgKJ} source={require("./Bottom Navigation.png")} resizeMode="cover"></ImageBackground></ImageBackground></ImageBackground></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
@@ -32,6 +32,13 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 20,
     top: 466
+  },
+  WnohOgKJ: {
+    width: 288,
+    height: 39,
+    position: "absolute",
+    left: 13,
+    top: 35
   }
 });
 export default Untitled3;
